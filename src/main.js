210d4f68dc5d5923ebdd2a610d3a0b1328d64e65
huyaosong <huyaosong@ewaresoft.com>
2024-03-26 01:08:25 +0800

    feat

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 import router from '@/router'
 import VConsole from 'vconsole'
 
+
+if (!HTMLCanvasElement.prototype.toBlob) {
+    console.log('poly-fill')
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+     value: function (callback, type, quality) {
+   
+       var binStr = atob( this.toDataURL(type, quality).split(',')[1] ),
+           len = binStr.length,
+           arr = new Uint8Array(len);
+   
+       for (var i=0; i<len; i++ ) {
+        arr[i] = binStr.charCodeAt(i);
+       }
+   
+       callback( new Blob( [arr], {type: type || 'image/png'} ) );
+     }
+    });
+   }
+
 new VConsole();
 
 const app = createApp(App)
